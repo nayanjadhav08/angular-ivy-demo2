0a619fdc47f2c0a9c101bcf45b84a244eabd94b8
nayanjadhav08 <nayanjadhav08@gmail.com>
2020-12-31 10:07:04 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { ScrollerComponent } from './scroller/scroller.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, ScrollerComponent, LoginComponent, RegistrationComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
