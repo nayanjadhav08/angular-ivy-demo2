f801144fbf332a1086e79551cfc42e80b60dedd1
nayanjadhav08 <nayanjadhav08@gmail.com>
2020-12-31 16:57:10 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,27 @@ import { Component, VERSION } from "@angular/core";
 })
 export class AppComponent {
   name = "Angular " + VERSION.major;
-
+  count = 1;
   arr = ["Apple", "mango", "banana", "abcd", "pqrst"];
-
+  img1 = "https://picsum.photos/id/1/700/500 ";
+  img2 = "https://picsum.photos/id/2/700/500 ";
+  img3 = "https://picsum.photos/id/3/700/500 ";
+  imgsrc = "";
   islogin = false;
 
+  btn1() {
+    this.imgsrc = this.img1;
+  }
+  btn2() {
+    this.imgsrc = this.img2;
+  }
+  btn3() {
+    this.imgsrc = this.img3;
+  }
   hello() {
     if (this.islogin == true) {
       this.islogin = false;
+      this.count++;
     } else {
       this.islogin = true;
     }
